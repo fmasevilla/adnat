a0b759de9ef1e62e1cae9433a988879fc8d815a9
Francis Sevilla <francissevilla0013@gmail.com>
2019-02-22 11:37:27 +0800

    Checks if the user is logged in or not.   *  redirects to organization index when logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    
+    redirect_to organization_index_path() if current_user.present?
   end
+
 end
